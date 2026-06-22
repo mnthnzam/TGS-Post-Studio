968c99327177f494b65a355679aee9ed38ea6e97
Manthan <manthan@zamstars.com>
2026-06-22 19:56:33 +0530

    Add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Shell from './app/Shell';
 import Login from './app/Login';
 import { cloudEnabled } from './cloud-config';
@@ -20,6 +21,7 @@ export default function App() {
     <>
       {content}
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
